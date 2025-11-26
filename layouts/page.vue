6ae7af16ffd8275d33fe07305411d4b051c474ad
MrTizu <204244194+skaindev@users.noreply.github.com>
2025-11-26 19:21:42 +0200

    Create page.vue

---
+++ layouts/page.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="app">
+    <AppBanner />
+    <AppHeader />
+    <Nuxt />
+    <!-- <AppFooter class="footer" /> -->
+    <AppCopyright />
+  </div>
+</template>
+
+<style scoped>
+.footer {
+  margin-top: 6rem;
+}
+</style>
