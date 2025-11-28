71b339283176a7264c2fc542a8f8675654e30f52
MrTizu <204244194+skaindev@users.noreply.github.com>
2025-11-28 05:52:16 +0200

    Create testnet.vue

---
+++ pages/testnet.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="page">
+    <TestnetHeader />
+    <main>
+      <TestnetHeroSection />
+      <TestnetOverviewSection />
+      <TestnetBuildSection />
+      <TestnetBugBountySection />
+    </main>
+    <TestnetFooter />
+  </div>
+</template>
+
+<script>
+export default {
+  head() {
+    return {
+      title: 'Testnet - Skaina',
+    }
+  },
+}
+</script>
+
+<style lang="postcss" scoped>
+@import url('../components/lib/media-query-helper.css');
+
+.page {
+  background: #000 no-repeat;
+  background-size: contain;
+  background-position: top;
+
+  @media (--viewport-small) {
+    background: #000 no-repeat;
+    background-size: 100%;
+  }
+}
+
+main {
+  overflow-x: hidden;
+}
+
+main > * + *,
+footer {
+  margin-top: 6rem;
+
+  @media (--viewport-medium) {
+    margin-top: 4rem;
+  }
+
+  @media (--viewport-small) {
+    margin-top: 2rem;
+  }
+}
+</style>
