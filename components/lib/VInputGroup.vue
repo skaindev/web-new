275a9e33a49d008c6c368598d1c4c841ddb4dbe3
MrTizu <204244194+skaindev@users.noreply.github.com>
2025-11-29 10:53:07 +0200

    Create VInputGroup.vue

---
+++ components/lib/VInputGroup.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="input-group">
+    <slot />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'VInputGroup',
+}
+</script>
+
+<style lang="postcss" scoped>
+.input-group {
+  display: flex;
+}
+.input-group *:not(:first-child) {
+  border-top-left-radius: 0;
+  border-bottom-left-radius: 0;
+}
+.input-group *:not(:last-child) {
+  border-top-right-radius: 0;
+  border-bottom-right-radius: 0;
+}
+</style>
